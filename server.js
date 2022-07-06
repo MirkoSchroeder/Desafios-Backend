4efbe5e1d9aed6a5590f905869027419c0aa1cd3
Mirko Schroeder <mirschroeder@hotmail.com>
2022-07-06 20:21:57 -0300

    Desafio 4 Mirko Schroeder

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express')
+const app = express()
+const { Router } = require('express')
+const router = Router()
+
+/* Productos */
+
+const Contenedor = require('./contenedor')
+const contenedor1 = new Contenedor('productos.json')
+
+/* Rutas */
+
+app.use('/api/productos', router)
+router.use(express.json())
+router.use(express.urlencoded({ extended: true }))
+
+app.use(express.static('public'))
+
+
+/* ------------------------- */
+
+router.get('/', (req, res) => {
+    const { id } = req.query
+    if (id != undefined) {
+        res.json(contenedor1.getById(Number(id)))
+    } else {
+        res.json(contenedor1.getAll())
+    }
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    res.json(contenedor1.getById(Number(id)))
+})
+
+router.post('/', (req, res) => {
+    const { nombre, precio, imagen } = req.body
+    if (nombre === '' || precio === '' || imagen === '') {
+        res.json({
+            error:"Falto rellenar algun campo"
+        })
+    } else {
+        res.json(contenedor1.save({nombre: nombre, precio: Number(precio), imagen: imagen}))
+    }
+})
+
+router.put('/:id', (req, res) => {
+    const prod = req.body
+    const { id } = req.params
+    res.json(contenedor1.update(Number(id), prod))
+})
+
+router.delete('/:id', (req, res) => {
+    const { id } = req.params
+    res.json(contenedor1.deleteById(Number(id)))
+})
+
+/* Servidor */
+
+const PORT = 8080;
+
+const server = app.listen(PORT, () => {
+  console.log(`Servidor HTTP escuchando en el puerto ${server.address().port}`);
+    });
+
+server.on("error", error => console.log(`Error en servidor: ${error}`));
